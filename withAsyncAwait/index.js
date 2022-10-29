39c001c89c431fe8147a1f895b9308277bff0d22
mysr3809 <myasar0938@gmail.com>
2022-10-29 15:56:27 +0200

    async-await implementation done

--- withAsyncAwait/index.js
+++ withAsyncAwait/index.js
@@ -1,27 +1,29 @@
 'use strict';
 
-const STEP_INTERVAL_MS = 50;
 const STEP_SIZE_PX = 10;
-const DANCE_TIME_MS = 5000;
+const STEP_INTERVAL_MS = 50;
+const DANCE_TIME_MS = 1000;
+const WALKING_CAT = 'http://www.anniemation.com/clip_art/images/cat-walk.gif';
 const DANCING_CAT_URL =
   'https://media1.tenor.com/images/2de63e950fb254920054f9bd081e8157/tenor.gif';
 
+function setTime(time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
+}
+
 async function walk(img, startPos, stopPos) {
   let catLeft = startPos;
-  const interval = setInterval(() => {
-    while (catLeft <= stopPos) {
-      img.style.left = catLeft + 'px';
-      catLeft += STEP_SIZE_PX;
-    }
-  }, STEP_INTERVAL_MS);
+  while (catLeft <= stopPos) {
+    img.style.left = catLeft + 'px';
+    catLeft += STEP_SIZE_PX;
+    await setTime(STEP_INTERVAL_MS);
+  }
 }
 
 async function dance(img) {
   img.src = DANCING_CAT_URL;
-  setTimeout(() => {
-    img.src = WALKING_CAT;
-    resolve('true');
-  }, DANCE_TIME_MS);
+  await setTime(DANCE_TIME_MS);
+  img.src = WALKING_CAT;
 }
 
 async function catWalk() {
@@ -36,7 +38,6 @@ async function catWalk() {
     await walk(img, startPos, centerPos);
     await dance(img);
     await walk(img, centerPos, stopPos);
-    catWalk();
   }
 }
 
